6b990c457686abab5d1ae069f59745182e90451b
vietma <viet.ma@gmail.com>
2016-06-20 23:23:06 +1000

    Initial Commit

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='yahoofinance',
+      version='0.1',
+      description='Yahoo Finance API',
+      url='http://github.com/yahoofinance',
+      author='Viet Ma',
+      packages=['yahoofinance'])
---
+++ yahoofinance/stock.py
@@ -0,0 +1,20 @@
+from urllib2 import Request, urlopen
+
+class Stock:     
+    
+    # make this private 
+    def __execute(self, symbol, criteria):
+        endpoint = 'http://download.finance.yahoo.com/d/quotes.csv?s=%s&f=%s' % (symbol, criteria)
+        request = Request(endpoint)
+        response = urlopen(request)
+        content = response.read().decode().strip()
+        return content
+        
+    def getLastTrade(self, symbol):
+        return self.__execute(symbol, 'l1')
+    
+    def getDayHigh(self, symbol):
+        return self.__execute(symbol, 'h')
+    
+    def getDayLow(self, symbol):
+        return self.__execute(symbol, 'g')
